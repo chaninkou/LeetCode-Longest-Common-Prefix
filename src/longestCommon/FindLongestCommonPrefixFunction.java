a42801df993d440f71612a17837a5ca49317d068
chaninkou <michaelkoulove@yahoo.com>
2019-11-21 14:20:08 -0800

    Great review on array and looping.

--- src/longestCommon/FindLongestCommonPrefixFunction.java
+++ src/longestCommon/FindLongestCommonPrefixFunction.java
@@ -13,7 +13,7 @@ public class FindLongestCommonPrefixFunction {
 
 		// Starting from 1 since
 		for (int i = 1; i < strs.length; i++) {
-			// startswith means if current is exactly in the strs[i]
+			// startswith means if current is exactly in the strs[i] without any other letters
 			// While loop will make sure we get a value that starts with current
 			while (!strs[i].startsWith(current)) {
 				current = current.substring(0, current.length() - 1);
@@ -31,4 +31,42 @@ public class FindLongestCommonPrefixFunction {
 
 		return current;
 	}
+	
+	// Another way to find it
+//	public String longestCommonPrefix(String[] strs){
+//		// The final result
+//		String longestPrefix = "";
+//		
+//		// If length is zero or strs is null
+//		if(strs.length == 0 || strs == null){
+//			return longestPrefix;
+//		}
+//		
+//		int index = 0;
+//		
+//		// Use first element to compare with others
+//		char[] firstWord = strs[0].toCharArray();
+//		
+//		for(char letter : firstWord){
+//			
+//			// Starting 1 because we are 0 index element to compare
+//			for(int i = 1; i < strs.length; i++){
+//				
+//				// If index >= strs[i] cause all element have the same prefix
+//				// Once we will an element letter does not match, return longestprefix right away
+//				if(index >= strs[i].length() || strs[i].charAt(index) != letter){
+//					return longestPrefix;
+//				}
+//			}
+//			
+//			// Update the index of first word, add the letter to the result
+//			index++;
+//			longestPrefix += letter;
+//		}
+//		
+//		// If there is not prefix, return nothing
+//		return longestPrefix;
+//	}
+	
+	
 }
